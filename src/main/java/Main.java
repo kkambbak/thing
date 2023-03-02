8f9f9431d3eb89ef1b538d0f49a36a73beee04d9
kkambbak <yunsung9841@gmail.com>
2023-03-02 10:36:04 +0900

    v8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -12,7 +14,7 @@ class App{
     public void run(){
         System.out.println("== 명언 앱 ==");
         int idx = 1;
-        ArrayList<Quotes> arr = new ArrayList<>();
+        Map<Integer,Quotes> qList = new HashMap<>();
 
         while(true){
             System.out.print("명령) ");
@@ -30,35 +32,54 @@ class App{
                 System.out.print("작가 : ");
                 String a = sc.nextLine();
                 System.out.println( idx + "번 명언이 등록되었습니다.");
-                Quotes newQuotes = new Quotes(idx++, s, a);
-                arr.add(newQuotes);
+                Quotes newQuotes = new Quotes(idx, s, a);
+                qList.put(idx++,newQuotes);
 
             }
 
-            if (order.equals("목록")){
+            if (order.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("------------------------");
-                for (Quotes q: arr
-                     ) {
-                    System.out.println(q.getQuotesNum()+ " / " + q.getAuthor() + " / " +  q.getQuotes());
+                qList.size();
+                for (int i = 1; i <= idx; i++) {
+                    Quotes q = qList.get(i);
+                    if (q != null)
+                        System.out.println(q.getQuotesNum() + " / " + q.getAuthor() + " / " + q.getQuotes());
                 }
             }
 
             if (order.contains("삭제?id=")){
                 int num = Integer.parseInt(order.substring(6));
-                boolean deleteCheck = false;
-                for (Quotes q: arr
-                ) {
-                    if(q.getQuotesNum()==num){
-                        arr.remove(q);
-                        System.out.println(num+"번 명언이 삭제되었습니다.");
-                        deleteCheck = true;
-                    }
+                Quotes q = qList.get(num);
+                if (q != null){
+
+                    qList.remove(num);
+                    System.out.println(num+"번 명언이 삭제되었습니다.");
                 }
-                if(deleteCheck == false){
+                else{
                     System.out.println(num + "번 명언이 존재하지 않습니다.");
                 }
+            }
 
+            if (order.contains("수정?id=")){
+                int num = Integer.parseInt(order.substring(6));
+
+
+                Quotes q= qList.get(num);
+                if(q != null){
+                    System.out.println("명언(기존) : " + q.getQuotes() );
+                    System.out.print("명언 : ");
+                    String newQ = sc.nextLine();
+                    q.setQuotes(newQ);
+                    System.out.println("작가(기존) : " + q.getAuthor() );
+                    System.out.print("작가 : ");
+                    String newA = sc.nextLine();
+                    q.setAuthor(newA);
+                    System.out.println(num + "번 명언이 수정되었습니다.");
+                }
+                else{
+                    System.out.println(num + "번 명언이 존재하지 않습니다.");
+                }
             }
         }
     }
@@ -73,6 +94,9 @@ class Orders{
 
 class Quotes{
     public int getQuotesNum() {
+        return quotesNum;
+    }
+    public int getQuotesNum(int i) {
         return quotesNum;
     }
 
