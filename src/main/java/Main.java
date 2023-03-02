380941bb86d2efb379daa75c52086e7f91046c1c
kkambbak <yunsung9841@gmail.com>
2023-03-02 09:21:13 +0900

    v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        App newApp = new App();
+        newApp.run();
+    }
+}
 
+class App{
+    public void run(){
+        System.out.println("== 명언 앱 ==\n" +
+                "명령) 종료");
     }
 }
