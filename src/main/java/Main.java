db25b238f8e99a22a9bc8a3115969c82369e9d31
kkambbak <yunsung9841@gmail.com>
2023-03-02 09:41:55 +0900

    v3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App newApp = new App();
@@ -7,7 +9,66 @@ public class Main {
 
 class App{
     public void run(){
-        System.out.println("== 명언 앱 ==\n" +
-                "명령) 종료");
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+        Scanner sc = new Scanner(System.in);
+        String order = sc.nextLine();
+        int idx = 1;
+
+        if (order.equals("등록") ){
+
+            System.out.print("명언 : ");
+            String s = sc.nextLine();
+
+            System.out.print("작가 : ");
+            String a = sc.nextLine();
+            System.out.println( idx + "번 명언이 등록되었습니다.");
+            Quotes newQuotes = new Quotes(idx++, s, a);
+
+        }
+        System.out.println("명령) 종료");
+    }
+}
+
+class Orders{
+    String orderName;
+    Orders(String orderName){
+        this.orderName = orderName;
+    }
+}
+
+class Quotes{
+    public int getQuotesNum() {
+        return quotesNum;
+    }
+
+    public void setQuotesNum(int quotesNum) {
+        this.quotesNum = quotesNum;
+    }
+
+    private int quotesNum;
+    private String quotes;
+
+    public String getQuotes() {
+        return quotes;
+    }
+
+    public void setQuotes(String quotes) {
+        this.quotes = quotes;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private String author;
+    Quotes(int quotesNum, String quotes, String author){
+        this.quotesNum = quotesNum;
+        this.quotes = quotes;
+        this.author = author;
     }
 }
